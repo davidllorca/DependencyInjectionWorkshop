cb75e8e15675b5d426448dec0885527324b2f349
David Llorca Baron <david.llorca@mango.com>
2018-07-22 21:35:56 +0200

    Refactor Injector class to me more abstract about client

--- app/src/main/java/me/davidllorca/diworkshop/common/di/Injector.java
+++ app/src/main/java/me/davidllorca/diworkshop/common/di/Injector.java
@@ -1,7 +1,12 @@
 package me.davidllorca.diworkshop.common.di;
 
-import me.davidllorca.diworkshop.ui.detail.DetailActivity;
-import me.davidllorca.diworkshop.ui.main.ListActivity;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import me.davidllorca.diworkshop.data.usecase.GetCharacterDetailUseCase;
+import me.davidllorca.diworkshop.data.usecase.GetCharactersUseCase;
+import me.davidllorca.diworkshop.ui.common.dialogs.DialogsManager;
+import me.davidllorca.diworkshop.ui.common.mvcviews.ViewMvcFactory;
 
 public class Injector {
 
@@ -12,26 +17,45 @@ public class Injector {
     }
 
     public void inject(Object client){
-        if(client instanceof ListActivity){
-            injectListActivity(((ListActivity) client));
-        } else if(client instanceof DetailActivity){
-            injectDetailActivity(((DetailActivity) client));
-        } else {
-            throw new IllegalArgumentException("Invalid client: " + client);
+        Class clazz = client.getClass();
+
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+            if(isPublicNonStaticNonFinal(field)){
+                injectField(client, field);
+            }
+        }
+
+    }
+
+    private boolean isPublicNonStaticNonFinal(Field field) {
+        int modifiers = field.getModifiers();
+        return Modifier.isPublic(modifiers) &&
+                !Modifier.isStatic(modifiers) &&
+                !Modifier.isFinal(modifiers);
+    }
+
+    private void injectField(Object client, Field field) {
+        try {
+            field.set(client, getServiceForClass(field.getType()));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private void injectListActivity(ListActivity client) {
-        client.mViewMvcFactory = mPresentationCompositionRoot.getMvcFactory();
-        client.mGetCharactersUseCase = mPresentationCompositionRoot.getGetCharactersUseCase();
-        client.mDialogsManager = mPresentationCompositionRoot.getDialogsManager();
-    }
-
-
-    private void injectDetailActivity(DetailActivity client) {
-        client.mViewMvcFactory = mPresentationCompositionRoot.getMvcFactory();
-        client.mGetCharacterDetailUseCase = mPresentationCompositionRoot.getGetCharacterDetailUseCase();
-        client.mDialogsManager = mPresentationCompositionRoot.getDialogsManager();
+    private Object getServiceForClass(Class<?> type) {
+        if(type.equals(DialogsManager.class)){
+            return mPresentationCompositionRoot.getDialogsManager();
+        } else if(type.equals(ViewMvcFactory.class)) {
+            return mPresentationCompositionRoot.getMvcFactory();
+        } else if(type.equals(GetCharactersUseCase.class)){
+            return mPresentationCompositionRoot.getGetCharactersUseCase();
+        } else if(type.equals(GetCharacterDetailUseCase.class)) {
+            return mPresentationCompositionRoot.getGetCharacterDetailUseCase();
+        } else {
+            return new RuntimeException("Unsupported service type class: " + type);
+        }
     }
 
 }
