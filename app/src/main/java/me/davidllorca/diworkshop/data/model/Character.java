f6f115ce191257d31e7b738341b2d520fb5288bd
David Llorca Baron <david.llorca@mango.com>
2018-07-22 20:35:47 +0200

    Add dependency of ImageLoader to DetailViewMvcImpl and new ImageView to layout

--- app/src/main/java/me/davidllorca/diworkshop/data/model/Character.java
+++ app/src/main/java/me/davidllorca/diworkshop/data/model/Character.java
@@ -54,4 +54,7 @@ public class Character {
         return gender;
     }
 
+    public String getImage() {
+        return image;
+    }
 }
--- app/src/main/java/me/davidllorca/diworkshop/ui/detail/DetailViewMvcImpl.java
+++ app/src/main/java/me/davidllorca/diworkshop/ui/detail/DetailViewMvcImpl.java
@@ -2,23 +2,29 @@ package me.davidllorca.diworkshop.ui.detail;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import me.davidllorca.diworkshop.R;
 import me.davidllorca.diworkshop.data.model.Character;
+import me.davidllorca.diworkshop.ui.common.ImageLoader;
 import me.davidllorca.diworkshop.ui.common.mvcviews.BaseViewMvc;
 
 public class DetailViewMvcImpl extends BaseViewMvc<DetailViewMvc.Listener> implements DetailViewMvc {
 
+    private final ImageLoader mImageLoader;
+    private ImageView mImageIv;
     private TextView mNameTv;
     private TextView mStatusTv;
     private TextView mSpeciesTv;
     private TextView mTypeTv;
     private TextView mGenderTv;
 
-    public DetailViewMvcImpl(LayoutInflater inflater, ViewGroup container) {
+    public DetailViewMvcImpl(LayoutInflater inflater, ViewGroup container, ImageLoader imageLoader) {
+        mImageLoader = imageLoader;
         setRootView(inflater.inflate(R.layout.activity_detail, container, false));
 
+        mImageIv = findViewById(R.id.iv_image_item);
         mNameTv = findViewById(R.id.tv_name_item);
         mStatusTv = findViewById(R.id.tv_status_item);
         mSpeciesTv = findViewById(R.id.tv_species_item);
@@ -29,6 +35,7 @@ public class DetailViewMvcImpl extends BaseViewMvc<DetailViewMvc.Listener> imple
 
     @Override
     public void bindCharacter(Character character) {
+        mImageLoader.loadImage(character.getImage(), mImageIv);
         mNameTv.setText(character.getName());
         mStatusTv.setText(character.getStatus());
         mSpeciesTv.setText(character.getSpecies());
