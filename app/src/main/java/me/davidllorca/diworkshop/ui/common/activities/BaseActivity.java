3a1b45a7ba120b3b5178d21700f6238d6fd89648
David Llorca Baron <david.llorca@mango.com>
2018-07-28 10:00:44 +0200

    Get PresentationComponent properly in BaseActivity

--- app/src/main/java/me/davidllorca/diworkshop/ui/common/activities/BaseActivity.java
+++ app/src/main/java/me/davidllorca/diworkshop/ui/common/activities/BaseActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 
 import me.davidllorca.diworkshop.MyApplication;
 import me.davidllorca.diworkshop.common.di.application.ApplicationComponent;
-import me.davidllorca.diworkshop.common.di.presentation.DaggerPresentationComponent;
 import me.davidllorca.diworkshop.common.di.presentation.PresentationComponent;
 import me.davidllorca.diworkshop.common.di.presentation.PresentationModule;
 
@@ -19,10 +18,7 @@ public class BaseActivity extends AppCompatActivity {
             throw new RuntimeException("There is no need to use injector more than once");
         }
         mIsInjectionDone = true;
-        return DaggerPresentationComponent.builder()
-                .presentationModule(new PresentationModule(this))
-                .applicationComponent(getApplicationComponent())
-                .build();
+        return getApplicationComponent().newPresentationComponent(new PresentationModule(this));
     }
 
     private ApplicationComponent getApplicationComponent() {
